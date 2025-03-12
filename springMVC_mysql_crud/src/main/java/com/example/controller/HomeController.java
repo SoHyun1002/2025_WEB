4d0ef9cdc3b84f77c756f2a4475f9ae260b4bba1
SoHyun1002 <ewela0367@gmail.com>
2025-03-12 18:44:09 +0900

    2025_03_12

--- springMVC_mysql_crud/src/main/java/com/example/controller/HomeController.java
+++ springMVC_mysql_crud/src/main/java/com/example/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -9,5 +12,25 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "redirect:/tasks";
+    }
+    
+    @GetMapping("/form2")
+    public String form2() {
+    	return "task/form2";
+    }
+    
+    @GetMapping("/result")
+    public String result(@RequestParam String name, Model model) {
+    	System.out.println(name);
+    	model.addAttribute("name", name);
+    	return "task/result";
+    }
+    
+    @PostMapping("/result")
+    public String result1(@RequestParam String name, Model model) {
+    	System.out.println(name);
+    	System.out.println("포스트 방식");
+    	model.addAttribute("name", name);
+    	return "task/result";
     }
 } 
